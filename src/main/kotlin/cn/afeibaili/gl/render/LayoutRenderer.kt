1984925a768d79e599c8a5f79723d54bc42d2d4b
Afei <afeibaili@outlook.com>
2026-08-20 21:18:28 +0800

    更新布局渲染，可渲染布局

--- src/main/kotlin/cn/afeibaili/gl/render/LayoutRenderer.kt
+++ src/main/kotlin/cn/afeibaili/gl/render/LayoutRenderer.kt
@@ -13,14 +13,18 @@ typealias Rect = cn.afeibaili.gl.render.layout.shape.Rectangle
  * @author AfeiBaili
  * @version 2026/8/16 13:09
  */
-class LayoutRenderer(val rectRenderer: RectangleRenderer, val rootLayout: Layout) : Closeable {
+class LayoutRenderer(val rectRenderer: RectangleRenderer, val rootLayout: Layout, val showLayout: Boolean = true) :
+    Closeable {
     private val logger = LoggerFactory.create("LayoutRenderer")
     val rectangles = mutableMapOf<String, Rect>()
+    val layouts = mutableListOf<Layout>()
 
     private fun match(layout: Layout) {
         for (component in layout.items) {
-            if (component is Layout) match(component)
-            else when (component) {
+            if (component is Layout) {
+                layouts.add(component)
+                match(component)
+            } else when (component) {
                 is Rect -> rectangles[component.key] = component
             }
         }
@@ -28,14 +32,29 @@ class LayoutRenderer(val rectRenderer: RectangleRenderer, val rootLayout: Layout
 
     fun init() {
         update()
+        logger.debug("layouts size: ${layouts.size}")
+        layouts.forEach { logger.debug(it) }
         logger.debug("rectangles size: ${rectangles.size}")
         rectangles.forEach { (_, value) -> logger.debug(value) }
     }
 
     fun update() {
         rectangles.clear()
+        layouts.clear()
         match(rootLayout)
 
+        if (showLayout) {
+            layouts.forEachIndexed { index, value ->
+                rectRenderer.put(
+                    index.toString(),
+                    value.absoluteX,
+                    value.absoluteY,
+                    value.width,
+                    value.height,
+                    value.backgroundColor
+                )
+            }
+        }
         rectangles.forEach { (key, value) ->
             rectRenderer.put(key, value.absoluteX, value.absoluteY, value.width, value.height, value.backgroundColor)
         }
