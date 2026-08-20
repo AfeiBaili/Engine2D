b0191a35f6038d50752aa9e388c0844dda98a17f
Afei <afeibaili@outlook.com>
2026-08-20 20:46:49 +0800

    处理窗口颠倒错误

--- src/main/kotlin/cn/afeibaili/gl/render/layout/weigth/WeightLayout.kt
+++ src/main/kotlin/cn/afeibaili/gl/render/layout/weigth/WeightLayout.kt
@@ -1,25 +1,45 @@
 package cn.afeibaili.gl.render.layout.weigth
 
+import cn.afeibaili.gl.render.layout.AbstractSetting
 import cn.afeibaili.gl.render.layout.Layout
+import cn.afeibaili.gl.render.layout.Setting
+import cn.afeibaili.gl.render.layout.align.AbstractAlignmentLayout
+import cn.afeibaili.gl.render.layout.align.AlignmentSetting
 
-fun Layout.rowWeight(setting: (WeightSetting) -> Unit = {}, action: RowWeight.() -> Unit): Layout {
-    val layout = RowWeight(this)
-    val set = WeightSetting()
-    setting(set)
-    set.apply(layout)
-    +layout
+fun AbstractWeightLayout.rowWeight(setting: (WeightSetting) -> Unit = {}, action: RowWeight.() -> Unit): Layout {
+    return buildLayoutRowWeight(this, WeightSetting().also { setting(it) }).also { action(it) }
+}
 
-    action(layout)
+fun AbstractWeightLayout.columnWeight(setting: (WeightSetting) -> Unit = {}, action: ColumnWeight.() -> Unit): Layout {
+    return buildLayoutColumnWeight(this, WeightSetting().also { setting(it) }).also { action(it) }
+}
+
+fun AbstractAlignmentLayout.rowWeight(setting: (AlignmentSetting) -> Unit, action: RowWeight.() -> Unit): Layout {
+    return buildLayoutRowWeight(this, AlignmentSetting().also { setting(it) }).also { action(it) }
+}
+
+fun AbstractAlignmentLayout.columnWeight(setting: (AlignmentSetting) -> Unit, action: ColumnWeight.() -> Unit): Layout {
+    return buildLayoutColumnWeight(this, AlignmentSetting().also { setting(it) }).also { action(it) }
+}
+
+fun Layout.rowWeight(setting: (Setting) -> Unit, action: RowWeight.() -> Unit): Layout {
+    return buildLayoutRowWeight(this, Setting().also { setting(it) }).also { action(it) }
+}
+
+fun Layout.columnWeight(setting: (Setting) -> Unit, action: ColumnWeight.() -> Unit): Layout {
+    return buildLayoutColumnWeight(this, Setting().also { setting(it) }).also { action(it) }
+}
+
+fun buildLayoutRowWeight(parent: Layout, setting: AbstractSetting<*>): RowWeight {
+    val layout = RowWeight(parent)
+    setting.apply(layout)
+    parent.append(layout)
     return layout
 }
 
-fun Layout.columnWeight(setting: (WeightSetting) -> Unit = {}, action: ColumnWeight.() -> Unit): Layout {
-    val layout = ColumnWeight(this)
-    val set = WeightSetting()
-    setting(set)
-    set.apply(layout)
-    +layout
-
-    action(layout)
+fun buildLayoutColumnWeight(parent: Layout, setting: AbstractSetting<*>): ColumnWeight {
+    val layout = ColumnWeight(parent)
+    setting.apply(layout)
+    parent.append(layout)
     return layout
 }
